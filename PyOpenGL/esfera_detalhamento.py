3bb8ee3a9a299975955ee52881753a873f5f81cd
André Alves de Araújo <andrealvesaraujo.1995@gmail.com>
2019-09-20 10:39:11 -0300

    Iniciando esfera_detalhamento

---
+++ PyOpenGL/esfera_detalhamento.py
@@ -0,0 +1,62 @@
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+ 
+import math
+
+n=50   
+m=50
+r=3
+	
+ 
+def esfera():
+	circulos = []
+	circulo= []
+	for i in range(0,n):
+		teta= i*(math.pi/(n-1)) - math.pi/2 
+		for j in range(0,m):
+			phi= (2*math.pi*j)/(m-1)
+
+			x = r*math.cos(teta)*math.cos(phi)
+			y = r*math.sin(teta)
+			z = r*math.cos(teta)*math.sin(phi)
+			
+			circulo+=[x,y,z]
+			#glVertex3fv([x,y,z])
+			
+		circulos+=circulo
+		break;
+	print(circulos)
+	glBegin(GL_POINTS)
+	
+	
+	glEnd()
+
+
+
+       
+def desenha():
+    global a
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glRotatef(2,0,1,0);
+    esfera()
+    glutSwapBuffers()
+    return;
+def timer(i):
+    glutPostRedisplay()
+    glutTimerFunc(50,timer,1)
+ 
+# PROGRAMA PRINCIPAL
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
+glutInitWindowSize(800,600)
+glutCreateWindow("Esfera Diferente")
+glutDisplayFunc(desenha)
+glEnable(GL_MULTISAMPLE)
+glEnable(GL_DEPTH_TEST)
+glClearColor(0.,0.,0.,1.)
+gluPerspective(45,800.0/600.0,0.1,50.0)
+glTranslatef(0.0,0.0,-12)
+#glRotatef(45,1,1,1)
+glutTimerFunc(50,timer,1)
+glutMainLoop()
