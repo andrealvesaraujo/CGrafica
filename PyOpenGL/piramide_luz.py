8c90a3d7822eeffcafbdab4e923ed0364c8a2836
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2019-10-17 17:41:15 -0300

    Termino piramide_luz

--- PyOpenGL/piramide_luz.py
+++ PyOpenGL/piramide_luz.py
@@ -5,39 +5,33 @@ from math import *
 import sys
 
 vertices = (
-	( 1,-1,-1),
-	( 1, 1,-1),
-	(-1, 1,-1),
-	(-1,-1,-1),
-	( 1,-1, 1),
-	( 1, 1, 1),
-	(-1,-1, 1),
-	(-1, 1, 1),
-	)
-
+    ( -1,0,1),
+    ( -1,0,-1),
+    ( 1,0,1),
+    ( 1,0,-1),
+    (0,4,0)
+    )
+ 
 linhas = (
-	(0,1),
-	(0,3),
-	(0,4),
-	(2,1),
-	(2,3),
-	(2,7),
-	(6,3),
-	(6,4),
-	(6,7),
-	(5,1),
-	(5,4),
-	(5,7),
-	)
-
+    (0,1),
+    (0,2),
+    (0,4),
+    (1,2),
+    (1,3),
+    (1,4),
+    (2,3),
+    (2,4),
+    (3,4)    
+    )
+ 
 faces = (
-	(0,1,2,3),
-	(3,2,7,6),
-	(6,7,5,4),
-	(4,5,1,0),
-	(1,5,7,2),
-	(4,0,3,6)
-	)
+    (0,1,2),
+    (0,1,4),
+    (0,2,4),
+    (1,3,4),
+    (1,2,3),
+    (2,3,4) 
+    )
 
 #https://www.opengl.org/wiki/Calculating_a_Surface_Normal
 #Begin Function CalculateSurfaceNormal (Input Triangle) Returns Vector
@@ -62,18 +56,27 @@ def calculaNormalFace(face):
     NLength = sqrt(N[x]*N[x]+N[y]*N[y]+N[z]*N[z])
     return ( N[x]/NLength, N[y]/NLength, N[z]/NLength)
 
-def Cubo():
-	glBegin(GL_QUADS)
+def Piramide():
+	glBegin(GL_TRIANGLES)
+	i = 0
 	for face in faces:
 		glNormal3fv(calculaNormalFace(face))
 		for vertex in face:
 			glVertex3fv(vertices[vertex])
+		i = i+1
 	glEnd()
+
+	glBegin(GL_LINES)
+	for linha in linhas:
+		for vertice in linha:
+			glVertex3fv(vertices[vertice])
+	glEnd()
+ 
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glRotatef(2,1,3,0)
-    Cubo()
+    glRotatef(2,6,4,0)
+    Piramide()
     glutSwapBuffers()
 
 def timer(i):
@@ -89,14 +92,13 @@ def reshape(w,h):
     gluLookAt(0,1,10,0,0,0,0,1,0)
 
 def init():
-#    mat_ambient = (0.0, 0.0, 0.5, 1.0)
-    mat_ambient = (0.4, 0.0, 0.0, 1.0)
+    mat_ambient = (0.8, 0.0, 0.0, 1.0)
     mat_diffuse = (1.0, 0.0, 0.0, 1.0)
-    mat_specular = (0.0, 1.0, 0.0, 1.0)
-    mat_shininess = (50,)
-    light_position = (0, 0, 0)
+    mat_specular = (1.0, 1.0, 1.0, 1.0)
+    mat_shininess = (100,)
+    light_position = (0, 8, 0)
     glClearColor(0.0,0.0,0.0,0.0)
-    glShadeModel(GL_FLAT)
+    glShadeModel(GL_SMOOTH)
 
     glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient)
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse)
@@ -112,7 +114,7 @@ def main():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
     glutInitWindowSize(800,600)
-    glutCreateWindow("Cubo")
+    glutCreateWindow("PiramideIluminada")
     glutReshapeFunc(reshape)
     glutDisplayFunc(display)
     glutTimerFunc(50,timer,1)
